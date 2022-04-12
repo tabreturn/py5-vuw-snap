9b36592225fdd77fc7036801b944a73321b772b6
tabreturn <github@tabreturn.com>
2022-04-13 08:46:10 +1200

    - change task 1 to static mode example

--- tasks/task_01.py
+++ tasks/task_01.py
@@ -1,13 +1,5 @@
-# module mode example
+# static mode example
 
-import py5
-
-def setup():
-    py5.size(300, 200)
-
-def draw():
-    py5.fill('#F00')
-    py5.circle(py5.mouse_x, py5.mouse_y, 10)
-
-py5.run_sketch()
-
+size(300, 200)
+fill('#F00')
+circle(width/2, height/2, 10)
